e316a51c85f5d83ab19a61a42e0ce6cb0325f22d
Pushkar A <asapure.pushkar.004@gmail.com>
2020-08-13 12:21:05 +0530

    First Commit

---
+++ railways/railways.js
@@ -0,0 +1,26 @@
+$('#category1').on('click',function(){
+    $("#subCategory1").removeClass('d-none');
+     $("#subCategory2").addClass("d-none");
+    $("#register-admin-tab").tab("show");
+	console.log($(this).id);
+})
+
+$("#category2").on("click", function () {
+    $("#subCategory1").addClass("d-none");
+    $("#subCategory2").removeClass("d-none");
+    $('#register-admin-tab').tab("show");
+    console.log($(this).id)
+});
+
+$("#subCategory1-project1").on('click', function(){
+    $("#register-university-tab").tab("show");
+});
+
+$("#zone1").on("click", function () {
+	$("#register-employer-tab").tab("show");
+});
+
+$("#division1, #division2").on("click", function () {
+    
+	window.open("./studentRegistration.html","_blank");
+});
